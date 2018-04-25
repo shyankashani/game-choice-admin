53cb8fae007acad5701e02ff4c3c8f09078a9cda
Shyan Kashani <shyankashani1@gmail.com>
2018-04-25 01:33:44 -0400

    Adds save inventory button

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -14,16 +14,17 @@
           <b-form-input
             placeholder="Location"
             size="sm"
-          >
-          </b-form-input>
+            v-model="selectedLocation"
+          ></b-form-input>
         </b-col>
 
         <b-col>
           <b-form-select
             size="sm"
+            v-model="selectedColorId"
           >
-            <option v-for="colorName in colorNames">
-              {{ colorName }}
+            <option v-for="color in colors" :value="color.colorId">
+              {{ color.name }}
             </option>
           </b-form-select>
         </b-col>
@@ -31,11 +32,21 @@
         <b-col>
           <b-form-select
             size="sm"
+            v-model="selectedCategoryId"
           >
-            <option v-for="categoryName in categoryNames">
-              {{ categoryName }}
+            <option v-for="category in categories" :value="category.categoryId">
+              {{ category.name }}
             </option>
           </b-form-select>
+        </b-col>
+
+        <b-col>
+          <b-button
+            type="submit"
+            variant="secondary"
+            size="sm"
+            v-on:click="saveInventory"
+          >Save</b-button>
         </b-col>
 
       </b-row>
@@ -46,6 +57,13 @@
   export default {
     name: 'Game',
     props: ['game', 'colors', 'categories'],
+    data: function () {
+      return {
+        selectedLocation: '',
+        selectedColorId: null,
+        selectedCategoryId: null
+      }
+    },
     computed: {
       gameName: function () {
         return this.game.name;
@@ -67,6 +85,13 @@
         return this.categories.map(category => {
           return category.name
         });
+      }
+    },
+    methods: {
+      saveInventory: function () {
+        console.log('this.selectedLocation', this.selectedLocation);
+        console.log('this.selectedColorId', this.selectedColorId);
+        console.log('this.selectedCategoryId', this.selectedCategoryId);
       }
     }
   }
